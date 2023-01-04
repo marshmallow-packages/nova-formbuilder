ea5d70059b6e2e9b0153a75aa43ac07248b3f248
LTKort <lars.kort94@gmail.com>
2023-01-04 14:02:09 +0100

    config init

--- config/nova-formbuilder.php
+++ config/nova-formbuilder.php
@@ -2,5 +2,25 @@
 
 // config for Marshmallow/NovaFormbuilder
 return [
+    'models' => [
+        'form' => Marshmallow\NovaFormbuilder\Models\Form::class,
+        'step' => Marshmallow\NovaFormbuilder\Models\Step::class,
+        'question' => Marshmallow\NovaFormbuilder\Models\Question::class,
+        'question_answer' => Marshmallow\NovaFormbuilder\Models\QuestionAnswer::class,
+        'question_answer_option' => Marshmallow\NovaFormbuilder\Models\QuestionAnswerOption::class,
+        'form_submission' => Marshmallow\NovaFormbuilder\Models\FormSubmission::class,
+    ],
+
+    // Get nova resources for this package
+    'resources' => [
+        'form' => Marshmallow\NovaFormbuilder\Nova\Form::class,
+        'step' => Marshmallow\NovaFormbuilder\Nova\Step::class,
+        'question' => Marshmallow\NovaFormbuilder\Nova\Question::class,
+        'question_answer' => Marshmallow\NovaFormbuilder\Nova\QuestionAnswer::class,
+        'question_answer_option' => Marshmallow\NovaFormbuilder\Nova\QuestionAnswerOption::class,
+        'form_submission' => Marshmallow\NovaFormbuilder\Nova\FormSubmission::class,
+    ],
+
+    'resolver' => Marshmallow\NovaFormbuilder\Nova\Resolvers\QuestionOptionResolver::class,
 
 ];
